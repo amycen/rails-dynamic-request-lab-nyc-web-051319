04e6e8b7c56a4ebf63ed7a42a0df5fc64daa4cd6
Amy Cen <amy.cen@blackrock.com>
2019-06-05 17:30:33 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,14 +2,8 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-<<<<<<< HEAD
 
   def show
     @student = Student.find(params[:id])
-=======
-  
-  def show
-    
->>>>>>> 6558af37bc95f510f9f8ad08ed9cfe581f276fc2
   end
 end
